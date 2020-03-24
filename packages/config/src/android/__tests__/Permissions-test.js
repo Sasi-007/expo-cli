09258bc5b0b7dbf5d91d9b6f180f569d7308dbd3
cruzach <charliecruzan@gmail.com>
2020-03-23 20:18:20 -0400

    add googleMapsApiKey config and tests

--- packages/config/src/android/__tests__/Permissions-test.js
+++ packages/config/src/android/__tests__/Permissions-test.js
@@ -1,50 +1,10 @@
 import { dirname, resolve } from 'path';
 import fs from 'fs-extra';
-import {
-  allPermissions,
-  getAndroidPermissions,
-  requiredPermissions,
-  setAndroidPermissions,
-} from '../Permissions';
+import { getAndroidPermissions, requiredPermissions, setAndroidPermissions } from '../Permissions';
 import { readAndroidManifestAsync } from '../Manifest';
 
 const fixturesPath = resolve(__dirname, 'fixtures');
 const sampleManifestPath = resolve(fixturesPath, 'react-native-AndroidManifest.xml');
-
-// TODO: use fixtures for manifest/mainactivity instead of inline strings
-
-const EXAMPLE_ANDROID_MANIFEST = `
-  <manifest xmlns:android="http://schemas.android.com/apk/res/android"
-      package="com.helloworld">
-      <application
-      android:name=".MainApplication"
-      android:theme="@style/AppTheme">
-
-      <uses-permission android:name="android.permission.INTERNET" />
-      <uses-permission android:name="android.permission.SYSTEM_ALERT_WINDOW"/>
-      <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
-      <uses-permission android:name="android.permission.WAKE_LOCK" />
-      <uses-permission android:name="com.google.android.c2dm.permission.RECEIVE" />
-      
-      <!-- OPTIONAL PERMISSIONS, REMOVE WHATEVER YOU DO NOT NEED -->
-        <uses-permission android:name="android.permission.VIBRATE" />
-        <uses-permission android:name="android.permission.READ_INTERNAL_STORAGE" />
-      <!-- END OPTIONAL PERMISSIONS -->
-
-      <activity
-          android:name=".MainActivity"
-          android:label="@string/app_name"
-          android:configChanges="keyboard|keyboardHidden|orientation|screenSize"
-          android:windowSoftInputMode="adjustResize">
-          <intent-filter>
-              <action android:name="android.intent.action.MAIN" />
-              <category android:name="android.intent.category.LAUNCHER" />
-          </intent-filter>
-        </activity>
-      <activity android:name="com.facebook.react.devsupport.DevSettingsActivity" />
-    </application>
-  </manifest>
-  `;
 
 describe('Android permissions', () => {
   it(`returns empty array if no android permissions key is provided`, () => {
